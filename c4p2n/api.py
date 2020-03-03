8c339274ddc6de46396094bd270b980e3c5ee311
Boger <kotvberloge@gmail.com>
2020-03-03 17:26:29 +0300

    :rotating_light: Add strict mypy+black and fix new typing/styling issues

--- c4p2n/api.py
+++ c4p2n/api.py
@@ -1,3 +1,8 @@
+from typing import (
+    Dict,
+    Any,
+)
+
 from fastapi import Depends, FastAPI
 
 from fastapi_security_typeform import SignatureHeader
@@ -8,11 +13,15 @@ from c4p2n.notion import Notion
 
 app = FastAPI()
 notion = Notion(token=config.NOTION_TOKEN, db_link=config.NOTION_LINK)
-signature_header_security = SignatureHeader(secret=config.WEBHOOK_SECRET.get_secret_value())
+signature_header_security = SignatureHeader(
+    secret=config.WEBHOOK_SECRET.get_secret_value()
+)
 
 
 @app.post("/call_for_paper")
-def call_for_paper_webhook(request: CallForPaperRequest, signature=Depends(signature_header_security)):
+def call_for_paper_webhook(
+    request: CallForPaperRequest, signature: str = Depends(signature_header_security)
+) -> Dict[str, Any]:
     answers = request.extract_answers()
     notion.add_talk_info(answers)
     return {"success": True}
--- c4p2n/extractors.py
+++ c4p2n/extractors.py
@@ -2,19 +2,19 @@ from typing import Dict, Any, List, Union
 
 
 def get_answer_type(answer: Dict[str, Any]) -> str:
-    return answer["type"]
+    return answer["type"]  # type: ignore  # will be fixed with #2
 
 
 def field_ref(answer: Dict[str, Any]) -> str:
-    return answer["field"]["ref"]
+    return answer["field"]["ref"]  # type: ignore  # will be fixed with #2
 
 
 def extract_text(text_answer: Dict[str, Any]) -> str:
-    return text_answer["text"]
+    return text_answer["text"]  # type: ignore  # will be fixed with #2
 
 
 def extract_choices(choices_answer: Dict[str, Any]) -> List[str]:
-    return choices_answer["choices"]["labels"]
+    return choices_answer["choices"]["labels"]  # type: ignore  # will be fixed with #2
 
 
 answer_type_to_extractor = {"text": extract_text, "choices": extract_choices}
@@ -22,5 +22,9 @@ answer_type_to_extractor = {"text": extract_text, "choices": extract_choices}
 
 def extract_answer(answer: Dict[str, Any]) -> Union[str, List[str]]:
     answer_type = get_answer_type(answer)
-    extractor = answer_type_to_extractor[answer_type]
-    return extractor(answer)
+    if answer_type == "text":
+        return extract_text(answer)
+    elif answer_type == "choices":
+        return extract_choices(answer)
+    else:
+        raise TypeError(f"Unexpected answer type: {answer_type}")
