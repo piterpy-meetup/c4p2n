37283f70b00688077a3fa84a4bff9fea39113d8a
sashabrud <shuhbrud@gmail.com>
2020-03-14 18:11:55 +0300

    :ambulance: Hotfix for timeout

--- c4p2n/telegram.py
+++ c4p2n/telegram.py
@@ -2,6 +2,6 @@ from ppm_telegram_bot_client.api_client import ApiClient, AsyncApis
 
 from c4p2n.config import config
 
-client = ApiClient(host=config.PPM_TELEGRAM_BOT_API_URL)
+client = ApiClient(host=config.PPM_TELEGRAM_BOT_API_URL, timeout=120)
 
-telegram_api = AsyncApis(client, timeout=120)
+telegram_api = AsyncApis(client)
